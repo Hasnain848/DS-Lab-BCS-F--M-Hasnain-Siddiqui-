204aeace89c88febb39767bf1c2041ac0795f3c9
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2025-08-29 13:22:03 +0500

    Update Q3.cpp

--- Lab 02/Q3.cpp
+++ Lab 02/Q3.cpp
@@ -1,37 +1,37 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int courses;
-    int *noOfstudent;
+    int student;
+    int *noOfcourses;
     int **jagged;
-    cout<<"Enter the no of courses: ";
-    cin>>courses;
-    noOfstudent = new int[courses];
-    jagged = new int*[courses];
-    cout<<"\nEnter the no of students enrolled in each course"<<endl;
-    for(int i=0;i<courses;i++){
-        cout<<"Course "<<i+1<<" : ";
-        cin>>noOfstudent[i];
-        jagged[i] = new int[noOfstudent[i]];
+    cout<<"Enter the no of student: ";
+    cin>>student;
+    noOfcourses = new int[student];
+    jagged = new int*[student];
+    cout<<"\nEnter the no of courses enrolled of  each student"<<endl;
+    for(int i=0;i<student;i++){
+        cout<<"Student "<<i+1<<" : ";
+        cin>>noOfcourses[i];
+        jagged[i] = new int[noOfcourses[i]];
     }
-    for(int i=0;i<courses;i++){
+    for(int i=0;i<student;i++){
         int sum=0;
-        cout<<"========Course "<<i+1<<" ========"<<endl;
-        for(int j=0;j<noOfstudent[i];j++){
-            cout<<"Student "<<j+1<<" marks: ";
+        cout<<"========Student "<<i+1<<" ========"<<endl;
+        for(int j=0;j<noOfcourses[i];j++){
+            cout<<"Course "<<j+1<<" marks: ";
             cin>>jagged[i][j];
            // sum+=jagged[i][j];
         }
        // double average = (double)sum/noOfstudent[i];
        // cout<<"Averge marks: "<<average<<endl;
     }
-    for(int i=0;i<courses;i++){
+    for(int i=0;i<student;i++){
         int sum=0;
-        cout<<"\n========Course "<<i+1<<" ========"<<endl;
-        for(int j=0;j<noOfstudent[i];j++){
+        cout<<"\n========Student "<<i+1<<" ========"<<endl;
+        for(int j=0;j<noOfcourses[i];j++){
             sum+=jagged[i][j];
         }
-        double average = (double)sum/noOfstudent[i];
+        double average = (double)sum/noOfcourses[i];
         cout<<"Average marks: "<<average<<endl;
     }
     //  for(int i=0;i<courses;i++){
@@ -42,9 +42,10 @@ int main(){
 
     //     }
     // }
-for(int i=0;i<courses;i++){
+for(int i=0;i<student;i++){
     delete[]jagged[i];
 }
 delete[] jagged;
+
 
 }
