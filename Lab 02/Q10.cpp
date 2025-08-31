d85b6329eccd8ae70ac96e7670d686de466dae66
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2025-08-31 13:55:45 +0500

    Add Q10

---
+++ Lab 02/Q10.cpp
@@ -0,0 +1,6 @@
+/*Task #10:
+Design a program that maintains library book records using dynamic arrays.
+=> Each row represents a category (e.g., Fiction, Science, History).
+=> Each category has a different number of books (use a jagged array).
+=> Store book IDs dynamically and allow the user to search for a given book ID.
+=> Print whether the book is available and in which category.*/
